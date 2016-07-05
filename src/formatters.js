a894be355dd61a2f971047baffd4d173b63e2d72
scott1028 <mic1028002@gmail.com>
2016-07-05 15:55:11 +0800

    Update formatters.js

--- src/formatters.js
+++ src/formatters.js
@@ -217,7 +217,7 @@ angular.module('sctPlugin')
             elm.blur(function(e) {
                 if(e.target.value.match(new RegExp(e.target.getAttribute('blur-pattern'))) === null){
                     if(message !== '') alert(message);
-                    else alert('當前輸入：' + e.target.value + '格式不符！');
+                    else alert('當前輸入值為："' + e.target.value + '" 與格式不符！');
                     angular.element(e.target).focus();
                 };
             });
