51fb25733f84d015b9dcd39948094395e8e0134c
scott.lan <scott.lan@taisys.com>
2016-07-21 12:04:00 +0800

    add utility method

---
+++ src/hacks/formaters.js
@@ -0,0 +1,32 @@
+'use strict';
+
+console.debug("doFormat API: <input class=\"form-control\" type=\"number\" ng-model=\"item.price\" ng-blur=\"doFormat('(?:^[0-9]{1,5}\\.\\d{1,2})|(?:^[0-9]{1,5})', 'item.price')\" />");
+var formaters = {
+    doFormat: function(regexString, nodeValue){
+        var findModel = function(node){
+            $$scope = $$scope[node];
+        };
+
+        var $scope = this;
+        var regexp = new RegExp(regexString);
+        var $$scope = $scope;
+        var tailField = null;
+        var tailNode = null;
+        var nodeList = nodeValue.split('.');
+
+        for(var i = 0; i < nodeList.length; i++){
+            if(i == (nodeList.length - 1)){
+                tailField = nodeList[i];
+                tailNode = $$scope;
+            }
+            else{
+                findModel(nodeList[i]);
+            }
+        };
+
+        if(tailNode[tailField]){
+            var stringValue = tailNode[tailField].toString();
+            tailNode[tailField] = parseFloat(stringValue.match(regexp));
+        }
+    }
+};
