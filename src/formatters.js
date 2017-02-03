0e19189dc14e74d7877e1797e1735829f8eb0114
scott.lan <scott.lan@taisys.com>
2017-02-03 11:40:12 +0800

    add input[text] <-> ngModel[number] 轉換

--- src/formatters.js
+++ src/formatters.js
@@ -324,4 +324,30 @@ angular.module('sctPlugin')
         };
     }
 ])
+.directive('stringToNumber', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(value) {
+        return parseFloat(value);
+      });
+      ngModel.$formatters.push(function(value) {
+        return '' + value;
+      });
+    }
+  };
+});
+.directive('numberToString', function() {
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(value) {
+        return '' + value;
+      });
+      ngModel.$formatters.push(function(value) {
+        return parseFloat(value);
+      });
+    }
+  };
+})
 ;
