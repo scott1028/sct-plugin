f479a3cad715385d90df1e3fdd02117860e16616
scott <scott.lan@taisys.com>
2017-02-09 22:24:57 +0800

    add ie support .startsWith for String

--- src/hacks/ie.detect.js
+++ src/hacks/ie.detect.js
@@ -44,3 +44,11 @@ console.debug('\
 \t    window.stop = stopFunctionForIE;\n\
 \t}\n\
 ');
+
+
+if(!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+        position = position || 0;
+        return this.substr(position, searchString.length) === searchString;
+    };
+};
