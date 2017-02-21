8d0f56767374df75724cf445a9b71d227934e407
scott.lan <scott.lan@taisys.com>
2017-02-21 15:34:07 +0800

    add animate ctrl

---
+++ src/hacks/ajax.animate.ctrl.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+var genAjaxAnimateCtrl = function(self){
+    self.ajaxing = true;
+    var ajaxCounter = 0;
+    self.addAjaxCounter = {
+        add: function(){
+            ajaxCounter += 1;
+        },
+        sub: function(){
+            ajaxCounter -= 1;  
+        }
+    };
+    if(self.$watch){
+        self.$watch(function(){
+            return ajaxCounter;
+        }, function(newValue, oldValue){
+            console.log(newValue, oldValue);
+            if(newValue === 0)
+                self.ajaxing = false;
+            else
+                self.ajaxing = true;
+        });
+    }
+    return self;
+};
